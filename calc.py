e7fdf0e1c1e3424127408dc5beb24edd0c78fe93
jungseo <dhkzmfwjd@gmail.com>
2018-03-06 15:40:03 +0900

    writing mimic_dict 1

--- calc.py
+++ calc.py
@@ -1,3 +1,12 @@
-Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:25:58) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print 'a'
+class Mimic(object):
+    m_dict={}
+    def mimic_dict(self, fn):
+        with open(fn,'r') as f:
+            words = f.read().lower().split()
+
+        
+    
+    return
+
+if __name__ = '__main__':
+    
